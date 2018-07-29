2e58ba5feb599e2c3eb161b0216007315b4a5ec0
Randee <rshirts@users.noreply.github.com>
2018-07-29 12:44:07 -0600

    worked on team stat alignment and arrows

--- src/components/Team/TeamStats/TeamStat/TeamStat.js
+++ src/components/Team/TeamStats/TeamStat/TeamStat.js
@@ -1,23 +1,27 @@
-import React from "react";
-import "./TeamStat.css";
-import EditableText from "../../../UI/EditableText/EditableText";
-import PropTypes from "prop-types";
+import React from 'react';
+import './TeamStat.css';
+import EditableText from '../../../UI/EditableText/EditableText';
+import PropTypes from 'prop-types';
+import IncDecArrows from '../../../UI/IncDecArrows/IncDecArrows';
 
-const teamStat = props => {
-  return (
-    <div styleName="teamStat">
-      <div styleName="statName">{props.statKey}</div>
-      <div styleName="statValue">
-        <EditableText changed={props.changed} content={props.content} />
-      </div>
-    </div>
-  );
+const teamStat = (props) => {
+	return (
+		<div styleName="statContainer">
+			<div styleName="teamStat">
+				<div styleName="statName">{props.statKey}</div>
+				<div styleName="statValue">
+					<EditableText changed={props.changed} content={props.content} />
+				</div>
+			</div>
+			<IncDecArrows />
+		</div>
+	);
 };
 
 teamStat.propTypes = {
-  changed: PropTypes.func,
-  statKey: PropTypes.string,
-  content: PropTypes.number
+	changed: PropTypes.func,
+	statKey: PropTypes.string,
+	content: PropTypes.number
 };
 
 export default teamStat;
--- src/components/UI/IncDecArrows/IncDecArrows.js
+++ src/components/UI/IncDecArrows/IncDecArrows.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './IncDecArrows.css';
+import Tooltip from '../../UI/Tooltip/Tooltip';
 
 const incDecArrows = () => {
 	return (
 		<div styleName="incDecArrows">
-			<div styleName="arrow up" />
-			<div styleName="arrow down" />
+			<Tooltip message="Increase Value">
+				<div styleName="arrow up" />
+			</Tooltip>
+			<Tooltip message="Decrease Value">
+				<div styleName="arrow down" />
+			</Tooltip>
 		</div>
 	);
 };
